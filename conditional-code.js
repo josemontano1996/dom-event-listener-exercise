2e33e7256ce9af8a3c4ffba569f5f716b660d3aa
Jose Manuel Montano Mengual <josemanuel1996enologia@gmail.com>
2022-11-22 18:27:39 +0100

    22.11.22

--- conditional-code.js
+++ conditional-code.js
@@ -7,4 +7,13 @@ if (myName === "Max") {
 const isLoggedIn = true;
 
 if (isLoggedIn) {
-    console.log('User is logged in')
+  console.log("User is logged in");
+} else if (!isLoggedIn) {
+  console.log("User is NOT logged in");
+}
+
+const enteredUserName = "Josema"; //empty sting or 0 are treated as false
+
+if (enteredUserName) {
+  console.log("Input is valid!");
+}
